ee19db316422487c02eb86591d5137b4f62ba004
Zachary Spalding <spalding@senylrc.org>
2019-02-28 12:24:01 -0500

    Update check_nest.sh

--- check_nest.sh
+++ check_nest.sh
@@ -7,6 +7,8 @@
 #3. Use Authorization URL to key pin code
 #4. Get the Authorization token
 #https://api.home.nest.com/oauth2/access_token?code={pincode}&client_id={your client id}&client_secret={your client secret}&grant_type=authorization_code
+#Get devie_id by doing this command
+# /usr/bin/curl  --location-trusted -H "Content-Type: application/json" -H "Authorization: Bearer <auhtorization toke>" -X GET https://developer-api.nest.com/devices/thermostats | sed -e 's/[{}]/''/g' |      awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'
 
 Auth="<auhtorization token"
 
