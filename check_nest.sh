37ee2ddc4b4b9d64cddf54fcbfd2df9562f8a3e0
Zachary Spalding <spalding@senylrc.org>
2019-02-28 12:16:58 -0500

    Create check_nest.sh

---
+++ check_nest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+#sample command ./check_nest2.sh <devid_id> ambient_temperature_f
+#1. Create Nest Developer Account
+#https://console.developers.nest.com/products
+#2. Create OAuth Cliet
+#3. Use Authorization URL to key pin code
+#4. Get the Authorization token
+#https://api.home.nest.com/oauth2/access_token?code={pincode}&client_id={your client id}&client_secret={your client secret}&grant_type=authorization_code
+
+Auth="<auhtorization token"
+
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+ echo "Missing parameters!"
+ echo "Usage: ./check_nest.sh device_id paramter"
+ exit 2
+fi
+
+status=$(curl  --location-trusted -H "Content-Type: application/json" -H "Authorization: Bearer $Auth"  https://developer-api.nest.com/devices/thermostats/$1"/"$2)
+
+if [ $status -ge 50 ] && [ $status -le 100 ]
+then
+ echo "Temp OK"
+ exit 0
+elif [ $status -lt 50 ]
+ then
+  echo "Temp to low"
+  exit 2
+elif [ $status -gt 100 ]
+  then
+   echo "Temp to high"
+   exit 2
+else
+ echo "Temp error"
+ exit 2
+fi
